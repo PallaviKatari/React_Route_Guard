9f41363bf59407dca2e06fcde82d0e1295c22d31
PallaviKatari <pallavikatari@gmail.com>
2022-05-09 22:18:23 +0530

    Commit_09.05.2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {
+  Link,
+  Routes,
+  Route,
+  useNavigate,
+  Navigate,
+  useLocation
+} from "react-router-dom";
+import AuthUser from "./AuthUser";
 
-function App() {
+const Home = () => <h1>Home (Public Route)</h1>;
+const About = () => <h1>About (Public Route)</h1>;
+
+const Dashboard = () => <h1>Dashboard (Private)</h1>;
+const Profile = () => <h1>Profile (Private)</h1>;
+
+const Login = () => {
+  const navigate = useNavigate();
+  const { login } = AuthUser();
+  const { state } = useLocation();
+
+  const handleLogin = () => {
+    login().then(() => {
+      navigate(state?.path || "/dashboard");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login</h1>
+      <div>
+      Login<br /><br />
+      <div>
+        Username<br />
+        <input type="text"/>
+      </div>
+      <div>
+        Password<br />
+        <input type="password"/>
+      </div>
     </div>
+      <button onClick={handleLogin}>Log in</button>
+    </div>
+  );
+};
+
+function Nav() {
+  const { authenticate, logout, login } = AuthUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+  const handleLogin = () => {
+    login();
+    navigate("/dashboard");
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+       
+        {/* <li>{!authenticate && <button onClick={handleLogin}>Login</button>}</li> */}
+      </ul>
+      {authenticate && <button onClick={handleLogout}>Logout</button>}
+    </nav>
   );
 }
 
-export default App;
+function RequireAuthentication({ children }) {
+  const { authenticate } = AuthUser();
+  const location = useLocation();
+
+  return authenticate === true ? (
+    children
+  ) : (
+    <Navigate to="/login" replace state={{ path: location.pathname }} />
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuthentication>
+              <Dashboard />
+            </RequireAuthentication>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <RequireAuthentication>
+              <Profile />
+            </RequireAuthentication>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
